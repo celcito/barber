9514554b442fcd14949be87015e60faa3f047bb5
Celso Ricardo Alves <celso_ricardo_25@hotmail.com>
2026-06-16 23:09:13 -0300

    fix: Resend instanciado dentro da função ao invés de module scope (causava 'new Headers' no build do Vercel)

--- lib/email.ts
+++ lib/email.ts
@@ -1,8 +1,6 @@
 import { Resend } from "resend";
 import { logger } from "./logger";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 interface SendEmailParams {
   to: string;
   subject: string;
@@ -27,6 +25,8 @@ export async function sendEmail({ to, subject, react }: SendEmailParams) {
     });
     return;
   }
+
+  const resend = new Resend(apiKey);
 
   await resend.emails.send({
     from,
